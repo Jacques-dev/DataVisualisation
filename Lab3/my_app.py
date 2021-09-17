9a29aadaaee4976a2917efd1287b9d2aca99aa28
Jacques <jacques.tellier@hotmail.com>
2021-09-17 09:48:40 +0200

    lab3 done

--- Lab3/my_app.py
+++ Lab3/my_app.py
@@ -7,9 +7,21 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 import streamlit as st
 import time
+import datetime
+import streamlit.components.v1 as components
 
 st.title("This is Jack's app")
 st.subheader('Welcome !')
+
+def log(func):
+
+    def wrapper(*args, **kwargs):
+        with open("logs.txt", "a") as f:
+            f.write("Called function : " + " ".join([str(arg) for arg in args]) + " at " + str(datetime.datetime.now()) + "\n")
+        val = func(*args, **kwargs)
+        return val
+
+    return wrapper
 
 # DATA IMPORTATION
 
@@ -74,31 +86,46 @@ if st.checkbox('Show dataframe'):
 
 "-----------------------------------------------------------------------"
 
-fig, ax = plt.subplots()
-chart = st.pyplot(fig)
-for i in range(20):
-    ax.hist(x=df["dom"], range=(0,i))
-    ax.set_xlabel("Date of the month")
-    ax.set_ylabel("Frequency")
-    chart.pyplot(fig)
-    time.sleep(0.1)
+@log
+@st.cache(suppress_st_warning=True)
+def superfunction1(name):
+    fig, ax = plt.subplots()
+    chart = st.pyplot(fig)
+    for i in range(20):
+        ax.hist(x=df["dom"], range=(0,i))
+        ax.set_xlabel("Date of the month")
+        ax.set_ylabel("Frequency")
+        chart.pyplot(fig)
+        time.sleep(0.1)
 
 "-----------------------------------------------------------------------"
 
 st.caption("DATA CROSS ANALYSIS")
 # DATA CROSS ANALYSIS
 
-fig, ax1 = plt.subplots()
-ax2 = ax1.twiny()
+@log
+@st.cache(suppress_st_warning=True)
+def superfunction2(name):
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twiny()
 
-ax1.hist(df["Lat"], range=(40.5, 41))
-fig.suptitle("")
-ax1.set_xlabel("Latitude")
-ax1.set_ylabel("Frequency")
+    ax1.hist(df["Lat"], range=(40.5, 41))
+    fig.suptitle("")
+    ax1.set_xlabel("Latitude")
+    ax1.set_ylabel("Frequency")
 
-ax2.hist(df["Lon"], range=(-74.3, -73.6))
-ax2.set_xlabel("Longitude")
-ax2.set_ylabel("Frequency")
-st.pyplot(fig)
+    ax2.hist(df["Lon"], range=(-74.3, -73.6))
+    ax2.set_xlabel("Longitude")
+    ax2.set_ylabel("Frequency")
+    st.pyplot(fig)
 
 "-----------------------------------------------------------------------"
+
+superfunction1("superfunction1")
+superfunction2("superfunction2")
+
+
+components.iframe("https://storage.googleapis.com/prod-phoenix-bucket/osp/cards/949/efrei-190523031051.jpg")
+
+components.iframe("https://media-exp1.licdn.com/dms/image/C4E1BAQG_c4p0ku2B7A/company-background_10000/0/1546960393793?e=2159024400&v=beta&t=3geGYrrDraTwwFulUuWPuYBHjxmJggj5BB0Pq4hoEPo")
+
